4e25cc67d0300148b0a5ecabe1daae20a9af90c9
Betvet <bsavet@hotmail.com>
2025-04-22 09:41:24 +0300

    Telegram entegrasyonu tamamlandı

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, request
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Sistem Rotası çalışıyor!"
+BOT_TOKEN = "7319641531:AAFxJObCtITv4KuwJH4i26fGgzZI8J0SmEg"  # Gerçek token
+CHAT_ID = "-1002615913345"  # Gerçek chat ID
 
-if __name__ == '__main__  ':
+@app.route('/', methods=["POST"])
+def webhook():
+    data = request.get_json()
+    message = data.get("message", "Mesaj bulunamadı")
+    send_message_to_telegram(message)
+    return {"status": "ok"}, 200
+
+def send_message_to_telegram(text):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": text
+    }
+    requests.post(url, json=payload)
+
+if __name__ == '__main__':
     app.run()
